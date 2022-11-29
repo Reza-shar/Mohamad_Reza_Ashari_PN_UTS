e33c532a7e8791bb74f46649374be7f067d70ad8
Reza-shar <113996651+Reza-shar@users.noreply.github.com>
2022-11-29 14:48:45 +0700

    Update SetengahLingkaran.java

--- src/com/lingkaran/SetengahLingkaran.java
+++ src/com/lingkaran/SetengahLingkaran.java
@@ -18,6 +18,7 @@ public class SetengahLingkaran implements Lingkaran {
     @Override
     public void hitungLuas() {
         double luas1 = (PANJANG / 2 * LEBAR) - (Math.PI * (PANJANG / 2)* (PANJANG / 2) / 4);
+        hasil = 2 * luas1;
     }
 
     @Override
